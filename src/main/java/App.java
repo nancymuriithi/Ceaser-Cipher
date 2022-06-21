b68aae67435d7a3c3cfe5034a421fe4170c6bd54
nancymuriithi <nmuriithi@gmail.com>
2022-06-21 21:56:43 +0300

    Add java file

---
+++ src/main/java/App.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+  public class App {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to Code Cipher!");
+        for( ;; ) {
+            System.out.println("choice\n 1.Encrypt\n 2.Decrypt\n3.Exit");
+            int choice = sc.nextInt();
+            Cipher caeser = new Cipher();
+            if (choice == 1) {
+                System.out.println("Enter the String for Encryption: ");
+                String message = new String();
+                message = sc.next();
+                System.out.println("Enter Key");
+                int shiftKey = sc.nextInt();
+                System.out.println(caeser.encrypt(message, shiftKey));
+
+            } else if (choice == 2) {
+                System.out.println("Enter the String for Decryption: ");
+                String message = new String();
+                message = sc.next();
+                System.out.println("Enter Key");
+                int shiftKey = sc.nextInt();
+                System.out.println(caeser.decrypt(message, 3));
+
+            } else if (choice == 3) {
+                System.exit(0);
+
+            } else {
+                System.out.println("Invalid choice");
+            }
+        }
+
+
+    }
+
+}
